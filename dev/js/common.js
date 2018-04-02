9625eb91d6a48bef05a57a4dcf2e58af15819627
rasul312312 <freeman190793@gmail.com>
2018-04-02 12:58:48 +0600

    chanche gulp js

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,20 +1,3 @@
-
-$(document).ready(function () {
-
-    // var NameVars = $('.name-class');
-
-    // // Маска для телефонов
-    // if (NameVars !== undefined && NameVars !== null) {
-    //     NameVars("(+996) 000-00-00-00");
-    // }
-
-}); // end document.ready
-
-
-// $(document).ajaxComplete(loadCommon);
-//     $(function () { loadCommon(); });
-
-//     function loadCommon() {
-//         // Писать common.js скрипты сюда
-//     }
-
+$(document).ready(function(){
+    
+});
