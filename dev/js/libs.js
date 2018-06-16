7f3476d8e0cb2b97e0b80dc4a5069f64e88c1f57
Akylai <osonovaakylai@gmail.com>
2018-06-16 18:34:00 +0600

    almost finish

--- dev/js/libs.js
+++ dev/js/libs.js
@@ -2,5 +2,5 @@
 @@include('../libs/uikit3/uikit.min.js')
 @@include('../libs/uikit3/uikit-icons.min.js')
 @@include('../libs/lightslider/lightslider.js')
-@@include('../libs/swiper/swiper.js')
-
+@@include('../libs/swiper/swiper.js') 
+@@include('../libs/fullpage/jquery.fullpage.min.js')
